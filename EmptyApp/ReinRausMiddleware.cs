97519c35877b962c579afb9c0380ed1fffb28e65
Albert Weinert <info@der-albert.com>
2017-05-03 18:08:07 +0200

    more logging

--- EmptyApp/ReinRausMiddleware.cs
+++ EmptyApp/ReinRausMiddleware.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 
 namespace EmptyApp
@@ -10,20 +11,28 @@ namespace EmptyApp
         private readonly RequestDelegate _next;
         private readonly IOptions<ReinRausOptions> _options;
         private readonly IWaitService _waitService;
+        private ILoggerFactory _loggerFactory;
+        private ILogger<ReinRausMiddleware> _globalLogger;
 
-        public ReinRausMiddleware(RequestDelegate next, IOptions<ReinRausOptions> options, IWaitService waitService)
+        public ReinRausMiddleware(RequestDelegate next, IOptions<ReinRausOptions> options, IWaitService waitService, ILoggerFactory factory)
         {
             _next = next;
             _options = options;
             _waitService = waitService;
+            _loggerFactory = factory;
+            _globalLogger = _loggerFactory.CreateLogger<ReinRausMiddleware>();
         }
 
         public async Task Invoke(HttpContext context)
         {
+            _globalLogger.LogInformation("Global: Invoking for {Nummer}", _options.Value.Nummer);
+            var logger = _loggerFactory.CreateLogger<ReinRausMiddleware>();
+            logger.LogWarning("local: rein");
             await context.Response.WriteAsync($"{_options.Value.Nummer} rein<br />");
             await _waitService.Wait();
             await _next(context);
             await _waitService.Wait();
+            logger.LogWarning("local: raus");
             await context.Response.WriteAsync($"{_options.Value.Nummer} raus<br />");
         }
     }
